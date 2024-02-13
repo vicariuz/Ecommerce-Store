5ac408b9b8d0020e30361095a518683d2a221f98
rasoh2 <sebastian.ortega.auriol@gmail.com>
2024-02-12 23:25:33 -0400

    mejora galeria, iniciar sesion 2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,22 +3,25 @@
 import "./Card.css";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
-import Context from '../context/context';
+import Context from "../context/context";
 
 const Card = ({ pizza }) => {
-  const { setPizza, setCart,  } = useContext(Context);
+  const { setPizza, setCart } = useContext(Context);
 
   const renderEstrellas = () => {
-    if (!pizza) return null; 
+    if (!pizza) return null;
 
     const calificacion = parseInt(pizza.rating, 10);
-    
 
     const estrellas = [];
     for (let i = 1; i <= 5; i++) {
       // Cambia la clase 'filled' si la calificación es mayor o igual a i
-      const claseEstrella = calificacion >= i ? 'filled' : '';
-      estrellas.push(<span key={i} className={`estrella ${claseEstrella}`}>★</span>);
+      const claseEstrella = calificacion >= i ? "filled" : "";
+      estrellas.push(
+        <span key={i} className={`estrella ${claseEstrella}`}>
+          ★
+        </span>
+      );
     }
     return estrellas;
   };
@@ -58,41 +61,44 @@ const Card = ({ pizza }) => {
   };
 
   return (
-    <div className="card shadow d-flex">
+    <div className='card shadow d-flex'>
       <img src={pizza.img} alt={pizza.name} />
-      <p id="subTitulo">{pizza.category}</p>
-      <h2 className="card-title text-capitalize mt-3">{pizza.name}</h2>
-      <div className="container-precio d-flex flex-column align-items-center">
-        <div className="precio d-flex justify-content-center align-items-center">
-          <p className="priceCard1">
-            Now {pizza.discountprice.toLocaleString("es-CL", {
+      <p id='subTitulo'>{pizza.category}</p>
+      <h2 className='card-title text-capitalize mt-3'>{pizza.name}</h2>
+      <div className='container-precio d-flex flex-column align-items-center'>
+        <div className='precio d-flex justify-content-center align-items-center'>
+          <p className='priceCard1'>
+            Now{" "}
+            {pizza.discountprice.toLocaleString("es-CL", {
               style: "currency",
               currency: "CLP",
             })}
-          </p> 
-          <p className="priceCard2">
-          {pizza.price.toLocaleString("es-CL", {
+          </p>
+          <p className='priceCard2'>
+            {pizza.price.toLocaleString("es-CL", {
               style: "currency",
               currency: "CLP",
             })}
           </p>
         </div>
-        <div className="calificacion">
-          <p>({pizza.rating}){renderEstrellas()}</p>
+        <div className='calificacion'>
+          <p>
+            ({pizza.rating}){renderEstrellas()}
+          </p>
         </div>
-        <div className="d-flex justify-content-evenly w-100">
-          <button className="btn btn-success" onClick={handleDetail}>
+        <div className='d-flex justify-content-evenly w-100'>
+          <button className='btn btn-success' onClick={handleDetail}>
             <img
-              src="/img/eyes-svgrepo-com.svg"
-              alt=""
+              src='/img/eyes-svgrepo-com.svg'
+              alt=''
               style={{ width: "30px", height: "30px", marginRight: "10px" }}
             />
             Ver más
           </button>
-          <button className="btn btn-danger" onClick={handleAdd}>
+          <button className='btn btn-danger' onClick={handleAdd}>
             <img
-              src="/img/cart-plus-svgrepo-com.svg"
-              alt=""
+              src='/img/cart-plus-svgrepo-com.svg'
+              alt=''
               style={{ width: "30px", height: "30px", marginRight: "10px" }}
             />
             Añadir
