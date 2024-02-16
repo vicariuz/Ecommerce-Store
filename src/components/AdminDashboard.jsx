34518d7354f7baf75f1ab3a00d24dad08e4d9cbb
jpbenvenuto <jpbenvenu@gmail.com>
2024-02-16 20:46:45 -0300

    Botones

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -7,19 +7,19 @@ const AdminDashboard = () => {
     return (
         <div className="dashboard">
             <form>
-            <h4>Dasboard Administrador</h4>
-              <b>Bienvenido Administrador explora diferentes opciones:</b>
-            <button type="button" className="btn btn-secondary btn-lg">
-            <Link to='/galleryadmin' className="link-button">   Galery y Editar Publicaciones </Link>
-            </button>
+                <h4>Dasboard Administrador</h4>
+                <b>Explora tus opciones:</b>
+              <button type="button" className="btn btn-secondary btn-lg">
+              <Link to='/galleryadmin' className="link-button">Galery y Editar Publicaciones</Link>
+              </button>
 
-            <button type="button" className="btn btn-secondary btn-lg">
-            <Link to='/crear' className="link-button"> Crear Nueva Publicacion</Link>
-            </button>
+              <button type="button" className="btn btn-secondary btn-lg">
+              <Link to='/crear' className="link-button">Crear Nueva Publicacion</Link>
+              </button>
 
-            <button type="button" className="btn btn-secondary btn-lg">
-            <Link to='/' className="link-button"> Volver a menu inicio</Link>
-            </button>
+              <button type="button" className="btn btn-secondary btn-lg">
+              <Link to='/' className="link-button">Volver a menu inicio</Link>
+              </button>
             </form>
         </div> 
       );
